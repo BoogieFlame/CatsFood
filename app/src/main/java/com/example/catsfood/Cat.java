2e183712ed8a34718ef508f7161a6afe3525f86b
BoogieFlame <kastor.vip@gmail.com>
2021-03-22 23:49:10 +0700

    Обновил модель котов

--- app/src/main/java/com/example/catsfood/Cat.java
+++ app/src/main/java/com/example/catsfood/Cat.java
@@ -1,12 +1,14 @@
 package com.example.catsfood;
 
 public class Cat {
+    public int id;
     public String name;
     public int age;
     public int weight;
     public boolean is_male;
 
-    Cat(String n, int a, int w, boolean i) {
+    Cat(int id, String n, int a, int w, boolean i) {
+        this.id = id;
         name = n;
         age = a;
         weight = w;
