634aa2160af416c25e2dd6e576cf300a871ee448
BoogieFlame <kastor.vip@gmail.com>
2021-03-22 22:36:27 +0700

    Добавил регистрацию в firebase и главное меню

---
+++ app/src/main/java/com/example/catsfood/MainMenu.java
@@ -0,0 +1,13 @@
+package com.example.catsfood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
