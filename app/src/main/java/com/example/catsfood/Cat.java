3619a14dbb755ff91607593c8c361389cf624de1
BoogieFlame <kastor.vip@gmail.com>
2021-03-22 22:57:03 +0700

    создал класс котов

---
+++ app/src/main/java/com/example/catsfood/Cat.java
@@ -0,0 +1,15 @@
+package com.example.catsfood;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int weight;
+    public boolean is_male;
+
+    Cat(String n, int a, int w, boolean i) {
+        name = n;
+        age = a;
+        weight = w;
+        is_male = i;
+    }
+}
