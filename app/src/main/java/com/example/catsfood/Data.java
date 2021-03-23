114363f579401721587d37ba6a30781cbe4f7123
BoogieFlame <kastor.vip@gmail.com>
2021-03-23 19:06:47 +0700

    Хранение данных и firestore

---
+++ app/src/main/java/com/example/catsfood/Data.java
@@ -0,0 +1,10 @@
+package com.example.catsfood;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
+public class Data {
+    public FirebaseAuth auth;
+    public List<Cat> mycat;
+}
